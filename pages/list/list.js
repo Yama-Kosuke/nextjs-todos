3c1153b64587eb3983aff969224b1f3400df9344
Yama-Kosuke <rag.rag.rag.kosuke0527@gmail.com>
2021-01-29 16:05:01 +0900

    Todo一覧及び削除済みTodoのページ作成＆Homeへの戻る

---
+++ pages/list/list.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+
+export default function List() {
+  return (
+    <>
+      <h1>Todo一覧</h1>
+      <h2>
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+      </h2>
+    </>
+    )}
