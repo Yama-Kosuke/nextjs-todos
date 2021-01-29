97e7b37b56d8a395bc267625d77bb898f1b32cf5
Yama-Kosuke <rag.rag.rag.kosuke0527@gmail.com>
2021-01-30 08:35:53 +0900

    component化させるためのパーツ作成

---
+++ pages/components/HeaderLine/HeaderLine.js
@@ -0,0 +1,60 @@
+// import React, { useState } from "react";
+// import Link from "next/link";
+// import Head from "next/head";
+// import clsx from "clsx";
+// import { makeStyles, useTheme, withStyles } from "@material-ui/core/styles";
+// import Drawer from "@material-ui/core/Drawer";
+// import Button from "@material-ui/core/Button";
+// import CssBaseline from "@material-ui/core/CssBaseline";
+// import AppBar from "@material-ui/core/AppBar";
+// import Toolbar from "@material-ui/core/Toolbar";
+// import List from "@material-ui/core/List";
+// import Typography from "@material-ui/core/Typography";
+// import Divider from "@material-ui/core/Divider";
+// import IconButton from "@material-ui/core/IconButton";
+// import MenuIcon from "@material-ui/icons/Menu";
+// import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+// import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+// import ListItem from "@material-ui/core/ListItem";
+// import ListItemIcon from "@material-ui/core/ListItemIcon";
+// import ListItemText from "@material-ui/core/ListItemText";
+// import Container from "@material-ui/core/Container";
+// import Table from "@material-ui/core/Table";
+// import TableBody from "@material-ui/core/TableBody";
+// import TableCell from "@material-ui/core/TableCell";
+// import TableContainer from "@material-ui/core/TableContainer";
+// import TableHead from "@material-ui/core/TableHead";
+// import TableRow from "@material-ui/core/TableRow";
+// import Paper from "@material-ui/core/Paper";
+// import Grid from "@material-ui/core/Grid";
+// import DateFnsUtils from "@date-io/date-fns";
+// import {
+//   MuiPickersUtilsProvider,
+//   KeyboardDatePicker,
+// } from "@material-ui/pickers";
+// import DeleteIcon from "@material-ui/icons/Delete";
+// import FormatListNumberedIcon from "@material-ui/icons/FormatListNumbered";
+
+
+
+// export const HeaderLine = () => {
+//   const classes = useStyles();
+  
+//   return (
+//   <>
+//       <Head>
+//         <title>Todo App</title>
+//       </Head>
+
+//         <CssBaseline />
+//         {/* 上部のバー構成 */}
+//         <AppBar
+//           position="static"
+//           className={clsx(classes.appBar, {
+//             [classes.appBarShift]: open,
+//           })}
+//           style={{ color: "#e0f2f1", backgroundColor: "#000" }}
+//         ></AppBar>
+//     <h1> test </h1>
+// </>)
+// };
